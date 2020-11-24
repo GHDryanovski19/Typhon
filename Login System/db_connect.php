db764a24a4e767c9c4044ae48d005042c5f392a7
Georgi Drianovski <GHDryanovski19@codingburgas.bg>
2020-11-24 23:32:20 +0200

    Add index file with the base game and first look, Reorganise folders, files and directories, Nuke old unusable files

--- Login System/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "website";
-$host = "127.0.0.1";
-
-$conn = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
-?>
---
+++ php/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "website";
+$host = "127.0.0.1";
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
+?>
