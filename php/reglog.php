6ef14a3af14a1a17f962751128f2bc2eb001f4e1
Georgi Drianovski <GHDryanovski19@codingburgas.bg>
2020-12-02 00:29:52 +0200

    Add verification to the sign up form, edit the looks of the sign up form

--- php/reglog.php
+++ php/reglog.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Sign Up</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login System</title>
+    <link href="../img/favicon.png" rel="shortcut icon"/>
     <link rel="stylesheet" href="../css/reglog.css">
     
 </head>
-<body>
+<body class="formBackground">
     
     <section class="left-section">
         <div id="left-cover" class="cover cover-hide">
-        <img src="../img/cover.png" alt="">
+        <img src="../img/cover.jpg" alt="">
             <h1>Welcome !</h1>
             <h3>Already have an account ?</h3>
             <button type="button" class="switch-btn" onclick="location.reload()">Login</button>
@@ -22,12 +23,13 @@
                 <input type="password" name="user-pass" class="input-box" placeholder="Password">
                 <input type="submit" name="login-btn" class="btn" value="Login">
             </form>
+            <button type="button" class="btn" onclick="goBack()">Go Back</button>
         </div>
     </section>
 
     <section class="right-section">
         <div id="right-cover" class="cover fade-in-element">
-        <img src="../img/cover.png" alt="">
+        <img src="../img/cover.jpg" alt="">
             <h1>Welcome !</h1>
             <h3>Don't have an account ?</h3>
             <button type="button" class="switch-btn" onclick="switchSignup()">Signup</button>
@@ -35,11 +37,18 @@
         <div id="right-form" class="form form-hide">
             <h1>Signup</h1>
             <form action="signup.php" method="post">
-                <input type="text" name="username" class="input-box" placeholder="User Name">
-                <input type="email" name="email" class="input-box" placeholder="Email">
-                <input type="password" name="password" class="input-box" placeholder="Password">
+                <input type="text" name="username" class="input-box" placeholder="User Name"
+                pattern=".{6,}" title="Username must contain at least 6 letters">
+                <input type="email" name="email" class="input-box" placeholder="Email"
+                pattern="[^@\s]+@[^@\s]+">
+                <input type="password" name="password" class="input-box" placeholder="Password"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                title="Must contain at least one number and one uppercase and lowercase letter, 
+                and at least 8 or more characters">
                 <input type="submit" name="signup" class="btn" value="Signup">
             </form>
+            <br>
+            <button type="button" class="btn" onclick="goBack()">Go Back</button>
         </div>
     </section>
 
